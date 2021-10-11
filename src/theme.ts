9ca3aecd2c0cd6f9ce61544132a6c42c9f4d3881
Farzad Yousefzadeh <farskid@gmail.com>
2021-10-11 12:12:55 +0300

    Fix the theme with proper config

--- src/theme.ts
+++ src/theme.ts
@@ -1,20 +1,20 @@
 import { extendTheme } from "@chakra-ui/react";
 
-const config = extendTheme({
-  initialColorMode: "dark",
-  useSystemColorMode: false,
+const theme = extendTheme({
+  config: {
+    initialColorMode: "dark",
+    useSystemColorMode: false,
+  },
   colors: {
     gray: {
       800: "#1F2024",
       500: "#8A8A99",
-      400: "#ACACB4"
-    }
+      400: "#ACACB4",
+    },
   },
   fonts: {
     heading: "TTCommons, Arial, sans-serif",
     body: "TTCommons, Arial, sans-serif",
-  }
-})
-
-const theme = extendTheme({ config })
-export default theme
+  },
+});
+export default theme;
