48be07a7b9b11554e5e6c154f897249fd6ca8e9d
Laura Kalbag <me@laurakalbag.com>
2021-10-12 13:39:23 +0100

    Wrap link around heading, not heading around link in posts list

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,24 +16,24 @@ const Home: NextPage<{ posts: Post[] }> = ({ posts }) => {
   return (
     <Layout posts={posts}>
       <Box padding={{ base: "3", md: "12" }} display="flex" flexDirection="column" align="center">
-        <Heading as="h1" padding="4" paddingBottom={{ base: "8", md: "12" }} align="left">Stately Engineering Blog</Heading>
+        <Heading as="h1" padding="4" paddingBottom={{ base: "8", md: "12" }} align="left" fontWeight="normal">Stately Engineering Blog</Heading>
         <List spacing="8" maxW="4xl" align="left">
           {posts.map((post) => (
             <ListItem key={post.id}>
-              <Heading size="2">
                 <Link href={post.slug} passHref>
                   <ChakraLink
                     padding="4"
                     _hover={{ bg: "whiteAlpha.100" }}
                     display="block"
                   >
-                    {post.title}{" "}
+                    <Heading size="2">
+                      {post.title}{" "}
+                    </Heading>
                     <HStack as="p" display="block" color="gray.500" size="smaller">
                       <span>{post.publishedAt}</span><span>by {post.author}</span>
                     </HStack>
                   </ChakraLink>
                 </Link>
-              </Heading>
             </ListItem>
           ))}
         </List>
