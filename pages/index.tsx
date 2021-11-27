866ce7563a84bf29845954b70b3d717e9527a870
Farzad Yousefzadeh <farskid@gmail.com>
2021-11-27 23:44:00 +0200

    fix invalid chakra prop warning

--- pages/index.tsx
+++ pages/index.tsx
@@ -37,7 +37,7 @@ const Home: NextPage<{ posts: Post[] }> = ({ posts }) => {
           >
             {title}
           </Heading>
-          <List spacing="4" maxW="3xl" align="left" listStyle="none">
+          <List spacing="4" maxW="3xl" align="left" listStyleType="none">
             {posts.map((post) => (
               <ListItem key={post.slug} marginTop="0">
                 {/* Formatting href makes server and client rendered hrefs consistent */}
@@ -48,12 +48,10 @@ const Home: NextPage<{ posts: Post[] }> = ({ posts }) => {
                     display="block"
                     color="gray.500"
                   >
-                    <Heading size="2" fontSize="2xl">{post.title} </Heading>
-                    <HStack
-                      as="p"
-                      display="block"
-                      color="gray.400"
-                    >
+                    <Heading size="2" fontSize="2xl">
+                      {post.title}{" "}
+                    </Heading>
+                    <HStack as="p" display="block" color="gray.400">
                       <span>{post.publishedAt}</span>
                       <span>by {post.author}</span>
                     </HStack>
